a9dedb7a4c3f830d2f2bc0be0390309f986c318c
wuhehao <hehao.wu@mail.utoronto.ca>
2017-11-18 23:41:04 -0500

    code affacted: FileManager.java implement the method to make it able to delete a tag from all the files and get a list of files according to their proposity.

--- phase1/src/TaggerSystem/FileManager.java
+++ phase1/src/TaggerSystem/FileManager.java
@@ -3,4 +3,43 @@ package TaggerSystem;
 import java.io.Serializable;
 
 public class FileManager implements Serializable {
+    private Folder folder;
+
+    public FileManager(Folder folder) {
+        this.folder = folder;
+    }
+
+    public void DeleteTag(Tag tag) {
+        for (ImageFile file: this.folder.value) {
+            ArrayList tag_list = file.getCurrentTagList();
+            if (tag_list.contains(tag)) {
+                clone = tag_list;
+                clone.remove(tag);
+                file.tags.add(clone);
+            }
+        }
+    }
+
+    public ArrayList<ImageFile> getFilesWithTag(Tag tag) {
+        FinalList = new ArrayList<ImageFile> ();
+        for (ImageFile file : this.folder.value) {
+            ArrayList tag_list = file.getCurrentTagList();
+            if (tag_list.contains(tag)) {
+                FinalList.add(file);
+            }
+        }
+        return FinalList;
+    }
+
+    public ArrayList<ImageFile> getAllFiles() {
+        FinalList = new ArrayList<ImageFile> ();
+        for (thing : this.folder) {
+            if (thing.isinstanceof(Imagefile)) {
+                FinalList.add(thing);
+            } else {
+                thing.getAllFiles();
+            }
+        }
+        return FinalList;
+    }
 }
