3f0054f514e8501c6a6eae90a41fad6421e5a50b
Jasper Chen <yuxiao.chen@mail.utoronto.ca>
2017-11-18 22:44:36 -0500

    Code affected: Folder.java
    Implement the instance varibles and constructor and a setter of parent.

--- phase1/src/TaggerSystem/Folder.java
+++ phase1/src/TaggerSystem/Folder.java
@@ -1,6 +1,21 @@
 package TaggerSystem;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Folder implements Serializable {
+    private Folder parent;
+    private ArrayList<Folder> children;
+    private ArrayList<ImageFile> value;
+    public Folder(ArrayList<Folder> children, ArrayList<ImageFile> value){
+        this.children = children;
+        this.value = value;
+        for (Folder child:this.children){
+            child.setParent(this);
+        }
+    }
+
+    public void setParent(Folder parent){
+        this.parent = parent;
+    }
 }
