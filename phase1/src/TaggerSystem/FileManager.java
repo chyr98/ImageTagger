0813cbc4031e34d6db4fae2891c632272111f89e
Jasper Chen <yuxiao.chen@mail.utoronto.ca>
2017-11-18 23:55:14 -0500

    A basic implementation of instance variable.

--- phase1/src/TaggerSystem/FileManager.java
+++ phase1/src/TaggerSystem/FileManager.java
@@ -3,4 +3,11 @@ package TaggerSystem;
 import java.io.Serializable;
 
 public class FileManager implements Serializable {
+
+    //The folder indicates where is the user working at on GUI.
+    private Folder currentFolder;
+
+    public Folder getCurrentFolder() {
+        return currentFolder;
+    }
 }
