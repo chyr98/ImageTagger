6b8a8b193921a3119a8af4e26f4dbcff7122071b
Artur <Artur@MacBook-Pro-Artur.local>
2017-11-19 01:32:07 -0500

    Restructured GUIMain and made an interface more readable and usable

--- phase1/src/GUI/GUIMain.java
+++ phase1/src/GUI/GUIMain.java
@@ -1,62 +1,22 @@
-package GUI;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-
 public class GUIMain extends Application {
-
-    private static Stage primaryStage;
-    private static AnchorPane mainLayout;
 
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        this.primaryStage = primaryStage;
-        primaryStage.setTitle("Photo Tager");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scenes/new_main_window.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Photo Tagger");
         primaryStage.setResizable(false);
-        primaryStage.setOnCloseRequest(event -> saving());
-
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(GUIMain.class.getResource("Scenes/StartScene.fxml"));
-        showScene(new Scene(loader.load()));
-
-    }
-
-    private void saving(){
-        File targetDirectory = OpenDirectoryChooser();
-    }
-
-    public static File OpenDirectoryChooser() {
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        return directoryChooser.showDialog(GUIMain.primaryStage);
-    }
-
-    public static void showScene(Scene scene) throws IOException {
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    public static void showStage(String source, String title) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(GUIMain.class.getResource(source));
-
-        Stage stage = new Stage();
-        stage.setTitle(title);
-        stage.setResizable(false);
-        stage.initModality(Modality.WINDOW_MODAL);
-        stage.initOwner(primaryStage);
-        stage.setScene(new Scene(loader.load()));
-        stage.showAndWait();
     }
 }
