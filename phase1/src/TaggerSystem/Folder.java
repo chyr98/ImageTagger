d0c47829c2b12de054740cfe0afd643acee74667
Jasper Chen <yuxiao.chen@mail.utoronto.ca>
2017-11-18 22:51:31 -0500

    Code affected: Folder.java
    Forgot to store the name of a folder, added instance variable "name" to store it.

--- phase1/src/TaggerSystem/Folder.java
+++ phase1/src/TaggerSystem/Folder.java
@@ -4,15 +4,22 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Folder implements Serializable {
+    private String name;
     private Folder parent;
     private ArrayList<Folder> children;
     private ArrayList<ImageFile> value;
-    public Folder(ArrayList<Folder> children, ArrayList<ImageFile> value){
+
+    public Folder(String name, ArrayList<Folder> children, ArrayList<ImageFile> value){
+        this.name = name;
         this.children = children;
         this.value = value;
         for (Folder child:this.children){
             child.setParent(this);
         }
+    }
+
+    public String getName(){
+        return name;
     }
 
     public void setParent(Folder parent){
