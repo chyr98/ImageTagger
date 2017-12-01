fb032a65b39b8ec9b2b9089647d9dfad4b7e59e4
Artur <Artur@user156-87.resnet.utoronto.ca>
2017-11-30 20:27:52 -0500

    Added undone Unit test file to the repository

---
+++ phase2/src/test/FolderTest.java
@@ -0,0 +1,78 @@
+package test;
+
+import TaggerSystem.FileDirectory;
+import TaggerSystem.Folder;
+import TaggerSystem.ImageFile;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class FolderTest {
+    // instance variables where all Testings could share
+    private Folder a, b;
+    private String name;
+    private ArrayList<Folder> children;
+    private ArrayList<ImageFile> value;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        name = System.getProperty("user.home");
+        a = new Folder(name, children, value);
+        a.getFileWithTag(tag);
+        b = new Folder(name, children, value);
+        b.getFileWithTag(tag);
+
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void addImage() throws Exception {
+    }
+
+    @org.junit.Test
+    public void deleteTag() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getFileWithTag() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getAllImages() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getChildren() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getValue() throws Exception {
+    }
+
+}
